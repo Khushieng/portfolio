2eb364a309cb7acbad3b0e37c31c9d301b5e57ad
Khushieng <engkhushi5@gmail.com>
2026-06-30 22:12:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const toggle = document.getElementById("theme-toggle");
+
+toggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    if(document.body.classList.contains("dark")){
+        toggle.textContent="☀️";
+    }else{
+        toggle.textContent="🌙";
+    }
+});
